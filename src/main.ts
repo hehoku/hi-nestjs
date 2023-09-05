8878afda51752944310de8a274f10893c3a8278e
hehoku <hehoku@outlook.com>
2023-09-05 22:49:18 +0800

    feat: add model, and init test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist will remove any properties that are not in the DTO
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
